60c07ee89a544c2b96a7eb4440d22ebf5a3d3a13
cplank <cplankshepard@gmail.com>
2019-07-12 12:49:12 -0700

    calendar works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Calendar from "./components/Calendar"
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Carrie's gonna run half a marathon</h1>
-
-      <Calendar></Calendar>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
